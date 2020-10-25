4862a9302ad2cd52a67364d921fc7cae744a79a6
yuliviaannisa <yuliviaannisa@gmail.com>
2020-10-25 17:47:17 +0700

    My List View

---
+++ 65MyIntentApp/app/src/main/java/com/example/a65myintentapp/MoveWithDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.a65myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+    }
+}
